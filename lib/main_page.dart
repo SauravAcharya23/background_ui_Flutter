57d28ca3095772f4b393ba6ee3246c138ece1937
SauravAcharya23 <sauravacharya236@gmail.com>
2025-08-08 13:21:04 +0545

    code cleaned

---
+++ lib/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrangeCurvedBackground extends StatefulWidget {
+  const OrangeCurvedBackground({super.key});
+
+  @override
+  State<OrangeCurvedBackground> createState() => _OrangeCurvedBackgroundState();
+}
+
+class _OrangeCurvedBackgroundState extends State<OrangeCurvedBackground> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
