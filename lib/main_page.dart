728e2801c43b8ec944ba3f5f09be75452dd6531e
SauravAcharya23 <sauravacharya236@gmail.com>
2025-08-08 13:29:56 +0545

    50% sized height divided with orange and white

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,6 +10,28 @@ class OrangeCurvedBackground extends StatefulWidget {
 class _OrangeCurvedBackgroundState extends State<OrangeCurvedBackground> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            ClipPath(
+              child: Container(
+                height: MediaQuery.of(context).size.height * .5,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFFF6B35), // Bright orange
+                      Color(0xFFFF8C42), // Lighter orange
+                    ]
+                  )
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
